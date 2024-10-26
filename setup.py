7a8ca5ccd2bac7a2e6167dda9b127a7389c42d8b
Honoluluchampi <msd13aj@gmail.com>
2024-10-26 17:50:06 +0900

    remove install-requires

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ ext_modules = [
 
 setup(
     name="DiffMITC3Layer",
+    version='0.0.1',
     packages=find_packages(),
     ext_modules=ext_modules,
-    install_requires=["numpy"],
+    install_requires=[],
     author="Riku TOYOTA",
     description="An implementation of the differentiable MITC3 element layer for Pytorch."
 )
